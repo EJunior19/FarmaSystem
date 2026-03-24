19768f196af98c6d75d00edbe501a8b128db3047
Junior Adolfo Enciso Galeano <jcorporativo357@gmail.com>
2026-03-24 19:44:24 -0300

    Proyecto FarmaSystem base

---
+++ src/main/java/com/farmasystem/FarmasystemApplication.java
@@ -0,0 +1,13 @@
+package com.farmasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmasystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmasystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmasystem/FarmasystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmasystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
